817f8e3d1daa3b19d8fbc86e8c773c46fc618224
fredcode <fredcode23@gmail.com>
2020-09-04 07:25:29 +0800

    login page prototype built

--- app/models.py
+++ app/models.py
@@ -8,3 +8,13 @@ class User(UserMixin, db.Model):
   email = db.Column(db.String(120), unique=True, nullable=False)
   name = db.Column(db.String(80), nullable=False)
   password = db.Column(db.String(100))
+  role = db.Column(db.String(50))
+
+
+class Blogpost(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(50))
+  subtitle = db.Column(db.String(100))
+  author = db.Column(db.String(20))
+  date_posted = db.Column(db.DateTime)
+  content = db.Column(db.Text)
