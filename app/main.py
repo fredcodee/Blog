12b6455747b20281a0d7ce47b92d2d2ac2cf515c
fredcode <fredcode23@gmail.com>
2020-09-06 02:34:01 +0800

    stripe webhook

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from sqlalchemy import or_, and_, desc
 
 #stripe payment for subscribers
 stripe_keys = {"secret_key": "sk_test_51HMfihKZXGIOAX0RFNz6yGAb9VXU9cRpv1bkIxjpRjLs8NZvXg14mAYRi3E5pbqryteqdPWuKNz2Z4DLzT1zBBLq004t4Ao7Ws", "publishable_key":"pk_test_51HMfihKZXGIOAX0RySSyQ9U4BduK4lgWYka31u02pvrSAqKk0OLtzgJ9CxjH2yQLTKHK1dZeUDaHbaFmotBJ1QZy00Vw3ZggI4",
-}
+"endpoint_secret": "whsec_AlCZzWqV9PIPWpwnpFYT7snN5EmrSHfJ"}
 stripe.api_key = stripe_keys["secret_key"]
 
 main = Blueprint('main', __name__)
@@ -54,3 +54,28 @@ def success():
 @main.route("/cancelled")
 def cancelled():
   return render_template("cancelled.html")
+
+#stripe webhook
+
+
+@main.route("/webhook", methods=["POST"])
+def stripe_webhook():
+  payload = request.get_data(as_text=True)
+  sig_header = request.headers.get("Stripe-Signature")
+
+  try:
+    event = stripe.Webhook.construct_event(payload, sig_header, stripe_keys["endpoint_secret"])
+
+  except ValueError as e:
+    # Invalid payload
+    return "Invalid payload", 400
+  except stripe.error.SignatureVerificationError as e:
+    # Invalid signature
+    return "Invalid signature", 400
+
+    # Handle the checkout.session.completed event
+  if event["type"] == "checkout.session.completed":
+    print("Payment was successful.")
+    #run some custom code here
+
+  return "Success", 200
